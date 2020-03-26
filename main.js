8502d89bb2b311a96cd8fc25b3db9e182647baab
Ivan Tomljenovic <tomljenovic21@gmail.com>
2020-03-26 19:51:58 +0000

    Connected Darksky API, added skycons and converter from Fahrenheit to Celsius

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 window.addEventListener("load", () => {
     let long;
     let lat;
+    let temperatureDescription = document.querySelector(".temperature-description");
+    let locationTimezone = document.querySelector(".location-timezone");
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let temperatureSection = document.querySelector(".temperature");
+    let temperatureSpan = document.querySelector('.temperature span');
+
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
@@ -17,9 +23,35 @@ window.addEventListener("load", () => {
                 })
                 .then(data => {
                     console.log(data);
+                    const {temperature, summary, icon} = data.currently;
+                    //Set DOM elements from the API
+                    temperatureDegree.textContent = temperature;
+                    temperatureDescription.textContent = summary;
+                    locationTimezone.textContent = data.timezone;
+                    //Formula for Celsius
+                    let celsius = (temperature - 32) * (5 / 9);
+                    
+                    //Set Icon
+                    setIcons(icon, document.querySelector('.icon'));
+
+                    //Change temperature to Celsius/Fahrenheit
+                    temperatureSection.addEventListener('click', () => {
+                        if(temperatureSpan.textContent === "F") {
+                            temperatureSpan.textContent = "C";
+                            temperatureDegree.textContent = Math.floor(celsius);
+                        } else {
+                            temperatureSpan.textContent ="F";
+                            temperatureDegree.textContent = temperature;
+                        }
+                    })
                 });
         });
-    } else {
-        h1.textContent = "Please enable you location permission";
-    };
+    } 
+
+    function setIcons(icon, iconID) {
+        const skycons = new Skycons({"color": "white"});
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
+    }
 });
